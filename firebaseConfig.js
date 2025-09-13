0709fcbe3a10844169d87f22725ac01c7358c040
Acer <madubhashinimanusha@gmail.com>
2025-09-14 01:11:10 +0530

    setup Firebase configuration and initialize services

---
+++ firebaseConfig.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import{getFirestore} from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdcd-GY212TeJWLp03u6gVkcemSVFLALM",
+  authDomain: "medicine-reminder-app-54657.firebaseapp.com",
+  projectId: "medicine-reminder-app-54657",
+  storageBucket: "medicine-reminder-app-54657.firebasestorage.app",
+  messagingSenderId: "791144219842",
+  appId: "1:791144219842:web:629b000b38eef8e9b4a75b",
+  measurementId: "G-KZCY9EXX9E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+
+export const db= getFirestore(app)
