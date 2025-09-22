5ed38dd2499098d0fb223deba23caddc2c3b00ca
Acer <madubhashinimanusha@gmail.com>
2025-09-22 22:02:49 +0530

    modify firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdcd-GY212TeJWLp03u6gVkcemSVFLALM",
-  authDomain: "medicine-reminder-app-54657.firebaseapp.com",
-  projectId: "medicine-reminder-app-54657",
-  storageBucket: "medicine-reminder-app-54657.firebasestorage.app",
-  messagingSenderId: "791144219842",
-  appId: "1:791144219842:web:629b000b38eef8e9b4a75b",
-  measurementId: "G-KZCY9EXX9E"
+ // add configutations
 };
 
 // Initialize Firebase
